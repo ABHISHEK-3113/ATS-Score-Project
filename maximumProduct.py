a1ca2c47d0eb0dcb805d74202540ed28b8f3d7a5
Abhishek <70990990+ABHISHEK-3113@users.noreply.github.com>
2024-07-18 18:59:07 +0530

    Update maximumProduct.py

--- maximumProduct.py
+++ maximumProduct.py
@@ -1,3 +1,6 @@
-def maximumProduct(self, nums):
+class Solution:
+    def maximumProduct(self, nums):
         nums.sort()
         return max(nums[-1] * nums[-2] * nums[-3], nums[0] * nums[1] * nums[-1])
+
+        
